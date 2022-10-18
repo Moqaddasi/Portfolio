ade11f06aad18c46ed877671f30c4171725054ec
Amirhosein <ah.moqaddasi@gmail.com>
2022-10-19 03:08:12 +0330

    update Resume and change primary font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     fontFamily: {
-      primary: 'Playfair Display',
+      primary: 'Work Sans',
       body: 'Work Sans',
     },
     container: {
